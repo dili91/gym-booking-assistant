d35658b4bb87dbb553b46b9ba6b52da8fae961fc
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-07-05 18:41:36 +0200

    todos

--- book/index.js
+++ book/index.js
@@ -6,7 +6,9 @@ exports.handler = async (event) => {
 
   //TODO: login
 
-  //TODO: book with OR without station
+  //TODO: get class detail. Need to poll till BookingInfo.bookingUserStatus is CanBook
+
+  //TODO: book
 
   //TODO: send out event with outcome
 };
